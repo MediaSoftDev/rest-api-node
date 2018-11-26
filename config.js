c23fd5ac0b33f30705795b4dd62be117290394da
Daniel <yourmediasoft@gmail.com>
2018-11-26 13:28:07 -0500

    Updating Commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports={
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://127.0.0.1/restapinodedb'
+    db: process.env.MONGODB_URI || 'mongodb://127.0.0.1/restapinodedb'
 }
