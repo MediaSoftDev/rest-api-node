f67fc64c36ef8d072d4529c2fe2d5fbd0757e4de
Daniel <yourmediasoft@gmail.com>
2018-11-27 09:15:13 -0500

    Configure cors

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+
 app.use('/api', api)
 
 app.get('/', (req,res)=>{
